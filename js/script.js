50be58c9a58ed727e23c851740fe15e27aef1f36
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-03-23 04:51:25 -0400

    Adding additional structure and interactivity. Starting styling the interface

--- js/script.js
+++ js/script.js
@@ -1,36 +1,72 @@
-const pokemon = document.querySelector('#search-input');
-const button = document.querySelector('#search-button');
+const searchPokemon = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
+const pokemonForm = document.querySelector('#pokemon-form');
 
 const getPokemon = async () => {
     try {
-        const searchPokemon = pokemon.value.toLowerCase();
-        const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchPokemon}`);
+        const pokemon = searchPokemon.value.toLowerCase();
+        const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemon}`);
         const data = await response.json();
         displayPokemon(data);
     } catch (err) {
+        clearPokemon();
         alert("Pokemon not found");
         console.log(err);
     }
 }
 
-const displayPokemon = (pokemon) => {
-    const pokemonName = document.querySelector('#pokemon-name');
-    const pokemonId = document.querySelector('#pokemon-id');
-    const pokemonWeight = document.querySelector('#pokemon-weight');
-    const pokemonHeight = document.querySelector('#pokemon-height');
-    const pokemonImage = document.querySelector('#sprite');
+const pokemonName = document.querySelector('#pokemon-name');
+const pokemonId = document.querySelector('#pokemon-id');
+const pokemonWeight = document.querySelector('#weight');
+const pokemonHeight = document.querySelector('#height');
+const pokemonImage = document.querySelector('#image-display');
+const pokemonType = document.querySelector('#types');
+const pokemonStats = document.querySelector('#stats');
+const pokemonHp = document.querySelector('#hp');
+const pokemonAttack = document.querySelector('#attack');
+const pokemonDefense = document.querySelector('#defense');
+const pokemonSpecialAttack = document.querySelector('#special-attack');
+const pokemonSpecialDefense = document.querySelector('#special-defense');
+const pokemonSpeed = document.querySelector('#speed');
 
+const displayPokemon = (pokemon) => {
     pokemonName.textContent = pokemon.name.toUpperCase();
     pokemonId.textContent = `#${pokemon.id}`;
     pokemonWeight.textContent = `Weight: ${pokemon.weight}`;
     pokemonHeight.textContent = `Height: ${pokemon.height}`;
     pokemonImage.innerHTML = `
-        <img 
-            src="${pokemon.sprites.front_default}"
-            alt="${pokemon.name}"
-            front default sprite
-        >
-    `
+        <img id="sprite" src="${pokemon.sprites.front_default}" alt="${pokemon.name} front_default" />
+    `;
+    pokemonType.innerHTML = pokemon.types.map(item => `<span id="type ${item.type.name}">${item.type.name}</span>`).join(' ');
+    pokemonHp.textContent = `${pokemon.stats[0].base_stat}`;
+    pokemonAttack.textContent = `${pokemon.stats[1].base_stat}`;
+    pokemonDefense.textContent = `${pokemon.stats[2].base_stat}`;
+    pokemonSpecialAttack.textContent = `${pokemon.stats[3].base_stat}`;
+    pokemonSpecialDefense.textContent = `${pokemon.stats[4].base_stat}`;
+    pokemonSpeed.textContent = `${pokemon.stats[5].base_stat}`;
 }
 
-button.addEventListener('click', getPokemon);
+const clearPokemon = () => {
+    const clearImage = document.querySelector('#sprite');
+    if (clearImage) {
+        clearImage.remove(); 
+    } 
+
+        pokemonId.textContent = '';
+        pokemonWeight.textContent = '';
+        pokemonHeight.textContent = '';
+        pokemonImage.innerHTML = '';
+        pokemonType.textContent = '';
+        pokemonHp.textContent = '';
+        pokemonAttack.textContent = '';
+        pokemonDefense.textContent = '';
+        pokemonSpecialAttack.textContent = '';
+        pokemonSpecialDefense.textContent = '';
+        pokemonSpeed.textContent = '';
+    
+}
+
+pokemonForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getPokemon();
+});
